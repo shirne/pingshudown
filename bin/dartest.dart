85ca58ed64ddd8f70d79cebaca350fdc8d6a5464
shirne <shirne@126.com>
2021-11-07 17:26:04 +0800

    增加help

--- bin/dartest.dart
+++ bin/dartest.dart
@@ -7,16 +7,24 @@ import 'download.dart';
 void main(List<String> arguments) async {
   const argId = 'id';
   const argStart = 'start';
+  const argHelp = 'help';
   final parser = ArgParser()
     ..addOption(argId, help: 'pingshu id', abbr: 'i')
-    ..addOption(argStart, help: 'start chapter', abbr: 'n', defaultsTo: '1');
+    ..addOption(argStart, help: 'start chapter', abbr: 'n', defaultsTo: '1')
+    ..addFlag(argHelp, help: 'show help message', abbr: 'h', negatable: false);
 
   final results = parser.parse(arguments);
+
+  if (results.wasParsed(argHelp)) {
+    stdout.write(parser.usage);
+    return;
+  }
 
   String? psId;
   if (results.wasParsed(argId)) {
     psId = results[argId].toString();
   } else {
+    stdout.writeln(parser.usage);
     stdout.writeln('Please input pingshu id:');
     psId = stdin.readLineSync();
     if (psId == null) {
@@ -27,4 +35,6 @@ void main(List<String> arguments) async {
   int start = int.parse(results[argStart].toString());
 
   await Download(psId).start(startId: start);
+  stdout.writeln('Download finished, please enter any key to exit...');
+  stdin.readLineSync();
 }
